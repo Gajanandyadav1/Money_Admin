318fdb3f9596dbcac519bbb669f5ad7564ac81f5
Gajanandyadav1 <132254405+Gajanandyadav1@users.noreply.github.com>
2025-05-26 12:14:29 +0530

    Update App.jsx

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -8,9 +8,9 @@ import Transaction from './Component/Transaction';
 import AllUser from './Component/UserSections/AllUser';
 import WalletBalance from './Component/UserSections/walletBalance';
 import AllActiveUsers from './Component/UserSections/AllActiveUsers';
-import AllinActive from './Component/UserSections/AllinActive';
-import BlockMember from './Component/UserSections/BlockMember';
-import PayoutSummary from './Component/PayoutSum/PayoutSummary';
+import AllinActive from './Component/UserSections/AllinActive' ;
+import BlockMember from './Component/UserSections/BlockMember' ;
+import PayoutSummary from './Component/PayoutSum/PayoutSummary' ;
 
 const App = () => {
   const [collapsed, setCollapsed] = useState(false);
@@ -18,11 +18,11 @@ const App = () => {
   return (
    <div className="container-fluid">
   <div className="row ">
-    <div className={collapsed ? 'col-auto sidebar-container px-0' : 'col-md-2 col-sm-3 sidebar-container px-0'}>
-      <Sidebar collapsed={collapsed} />
+    <div className={collapsed ? 'col-auto sidebar-container px-0 ' : 'col-md-2 col-sm-3 sidebar-container px-0'}>
+      <Sidebar collapsed={collapsed}  />
     </div>
 
-    <div className={collapsed ? 'col ps-0' : 'col-md-10 col-sm-9 g-0 g-1'}>
+    <div className={collapsed ? 'col ps-0' : 'col-md-10 col-sm-9 g-0 g-1 '}>
       <Header toggleSidebar={() => setCollapsed(!collapsed)} />
   
          <Routes>
@@ -34,7 +34,7 @@ const App = () => {
         <Route path="/activein" element={<AllinActive/>}/>
         <Route path="/blockusers" element={<BlockMember/>}/>
         <Route path="/payout" element={<PayoutSummary/>}/>
-       
+ 
         </Routes> 
     </div>
   </div>
