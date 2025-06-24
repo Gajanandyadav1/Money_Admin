e28332e4727029927774a45d48c1bb240222d587
Gajanandyadav1 <132254405+Gajanandyadav1@users.noreply.github.com>
2025-06-24 10:55:29 +0530

    Update AllRoute.jsx

--- src/AllRoute.jsx
+++ src/AllRoute.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from  './Component/Home'; 
 import SideBar from './Component/SideBar' ;
-import App from './App' ;
+import App from './App'  ;
 
 
 
